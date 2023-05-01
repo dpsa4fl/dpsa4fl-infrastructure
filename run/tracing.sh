e670a686a6b186a520dc047e4ac1a45a6c235970
Maxim Urschumzew <u.maxim@live.de>
2023-05-01 07:33:09 +0200

    Delete old config files.

--- run/tracing.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e; set -o pipefail;
-
-echo "starting jaeger..."
-
-docker run -d -p6831:6831/udp -p6832:6832/udp -p16686:16686 -p14268:14268 jaegertracing/all-in-one:latest
-
