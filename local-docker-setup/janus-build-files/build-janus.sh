628994404e5a7c7a19c541d4891497d83c7200fe
Maxim Urschumzew <u.maxim@live.de>
2023-09-27 14:00:48 +0200

    Update deps and build script.

--- local-docker-setup/janus-build-files/build-janus.sh
+++ local-docker-setup/janus-build-files/build-janus.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 function build
 {
-    BUILDKIT_PROGRESS=plain DOCKER_BUILDKIT=1 docker build ../janus/ -f Dockerfile.dev --build-arg BINARY=$1 -t janus_$1
+    BUILDKIT_PROGRESS=plain DOCKER_BUILDKIT=1 docker build "${SCRIPT_DIR}/../janus/" -f "${SCRIPT_DIR}/Dockerfile.dev" --build-arg BINARY=$1 -t janus_$1
 }
 
 build "aggregator"
